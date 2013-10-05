a28ae6ee80b8053bf2001c4611f20f0b73d82818
Daniel <daniel@ubuntu.ubuntu-domain>
2013-10-05 11:15:24 +0100

    Workshops are now used for automated crafting

--- town_item.lua
+++ town_item.lua
@@ -41,16 +41,22 @@ minetest.register_node("castle:workbench",{
 	paramtype = "light",
 	groups = {choppy=2,oddly_breakable_by_hand=2,flammable=2},
 	drawtype = "normal",
-	on_construct = function(pos)
-		local meta = minetest.get_meta(pos)
-		meta:set_string("formspec",
-				"size[8,9]"..
-				"list[current_name;main;0,1;8,4;]"..
-				"list[current_player;main;0,5;8,4;]")
-		meta:set_string("infotext", "Workbench")
-		local inv = meta:get_inventory()
-		inv:set_size("main", 8*3)
-	end,
+    on_construct = function ( pos )
+        local meta = minetest.get_meta( pos )
+		meta:set_string( 'formspec', 'size[10,10;]' ..
+			'label[1,0;Source Material]' ..
+			'list[context;src;1,1;2,4;]' ..
+			'label[4,0;Recipe to Use]' ..
+			'list[context;rec;4,1;3,3;]' ..
+			'label[7.5,0;Craft Output]' ..
+			'list[context;dst;8,1;1,4;]' ..
+			'list[current_player;main;1,6;8,4;]' )
+        meta:set_string( 'infotext', 'Workbench' )
+        local inv = meta:get_inventory()
+        inv:set_size( 'src', 2 * 4 )
+		inv:set_size( 'rec', 3 * 3 )
+		inv:set_size( 'dst', 1 * 4 )
+    end,
 	can_dig = function(pos,player)
 		local meta = minetest.get_meta(pos);
 		local inv = meta:get_inventory()
@@ -69,6 +75,83 @@ minetest.register_node("castle:workbench",{
 				" takes stuff from workbench at "..minetest.pos_to_string(pos))
 	end,
 })
+local get_recipe = function ( inv )
+	local result, needed, input
+	needed = inv:get_list( 'rec' )
+
+	result, input = minetest.get_craft_result( {
+		method = 'normal',
+		width = 3,
+		items = needed
+	} )
+
+	local totalneed = {}
+
+	if result.item:is_empty() then
+		result = nil
+	else
+		result = result.item
+		for _, item in ipairs( needed ) do
+			if item ~= nil and not item:is_empty() and not inv:contains_item( 'src', item ) then
+				result = nil
+				break
+			end
+			if item ~= nil and not item:is_empty() then
+				if totalneed[item:get_name()] == nil then
+					totalneed[item:get_name()] = 1
+				else
+					totalneed[item:get_name()] = totalneed[item:get_name()] + 1
+				end
+			end
+		end
+		for name, number in pairs( totalneed ) do
+			local totallist = inv:get_list( 'src' )
+			for i, srcitem in pairs( totallist ) do
+				if srcitem:get_name() == name then
+					local taken = srcitem:take_item( number )
+					number = number - taken:get_count()
+					totallist[i] = srcitem
+				end
+				if number <= 0 then
+					break
+				end
+			end
+			if number > 0 then
+				result = nil
+				break
+			end
+		end
+	end
+
+	return needed, input, result
+end
+
+minetest.register_abm( {
+	nodenames = { 'castle:workbench' },
+	interval = 5,
+	chance = 1,
+	action = function ( pos, node )
+		local meta = minetest.get_meta( pos )
+		local inv = meta:get_inventory()
+		local cresult, newinput, needed
+		if not inv:is_empty( 'src' ) then
+			-- Check for a valid recipe and sufficient resources to craft it
+			needed, newinput, result = get_recipe( inv )
+			if result ~= nil and inv:room_for_item( 'dst', result ) then
+				inv:add_item( 'dst', result )
+				for i, item in pairs( needed ) do
+					if item ~= nil and item ~= '' then
+						inv:remove_item( 'src', ItemStack( item ) )
+					end
+					if newinput[i] ~= nil and not newinput[i]:is_empty() then
+						inv:add_item( 'src', newinput[i] )
+					end
+				end
+			end
+		end
+	end
+} )
+
 
 local function has_locked_chest_privilege(meta, player)
 	if player:get_player_name() ~= meta:get_string("owner") then
